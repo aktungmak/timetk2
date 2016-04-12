2d2ae563309b938d184fefb2662420fc3e60a267
Joshua Green <greenj@ETVUK0075.TANDBERGTV.com>
2016-04-12 09:40:21 +0100

    user creation and login/logout functionality is now working!
    just need to tidy up presentation so it looks nice.

--- ttk2/urls.py
+++ ttk2/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^register/', views.register, name='create_account'),
 
     url(r'^$', views.activities, name='index'),
     url(r'^activities', views.activities, name='activities'),
